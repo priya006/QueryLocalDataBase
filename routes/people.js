fb3033d7ae97c8b802a0fbbbca2afad0021039be
pboopathi <pboopathi@ebay.com>
2020-09-15 00:32:26 -0700

    Somewhat working code

--- routes/people.js
+++ routes/people.js
@@ -2,14 +2,19 @@
 const express = require("express");
 const Router = express.Router();
 const mysqlConnection = require("../connection");
+const path = require("path");
 
 
 Router.get("/",(req, res)=>{
      // query to the database and get the records
-mysqlConnection.query("SELECT * from people",(err,rows,fields) =>{
+mysqlConnection.query("SELECT * from people",function (err,data){
   if(!err){
-    res.send(rows);
-      console.log(res);
+   // res.send(rows);
+      console.log(data);
+     // res.send(data);
+
+     res.sendFile(path.join(__dirname + '/index_new.html'));
+      
   }
 
   else
@@ -20,7 +25,40 @@ mysqlConnection.query("SELECT * from people",(err,rows,fields) =>{
     
 } )
 
-})
+});
+
+
+Router.post("/Employees",(req, res)=>{
+    // query to the database and get the records
+
+    let age = req.body.age;
+    console.log('Age', age);
+
+
+mysqlConnection.query("INSERT INTO people VALUES (12434,'xbc')",function (err,data){
+ if(!err){
+  // res.send(rows);
+     console.log(data);
+    //Sends data to /Employees page
+     res.send(data);
+
+   
+     
+ }
+
+ else
+ {
+     console.log(err);
+     console.log(res);
+ }
+   
+} )
+
+});
+
+
+//close database connection
+
 
 
 module.exports = Router;
